33f78397da6c0617db3620ee0a2df6f5de68d8e6
srikavipriyan-ma2021 <srikavipriyan.ma2021@vitstudent.ac.in>
2024-02-08 18:11:45 +0530

    ---

--- rought.py
+++
@@ -1,6 +0,0 @@
-import sys
-import numpy as np
-
-# print(sys.argv)
-foldroom1mix001ov1 = np.load(r"DCASE2020_SELD_dataset\feat_label\mic_dev\fold1_room1_mix001_ov1.npy")
-print(len(foldroom1mix001ov1[0]))
